a8bf124e32d25853e1e517483ad396e20273dae3
mokeyCode <sanylanv@gmail.com>
2024-12-19 15:43:57 +0800

    fix

--- app/trade/page.tsx
+++ app/trade/page.tsx
@@ -87,6 +87,7 @@ const Trade = () => {
   const [amount, setAmount] = useState('')
   const [slideValue, setSlideValue] = useState(78)
   const [confirmBuy, setConfirmBuy] = useState(false)
+  const [toSuccessPage, setToSuccessPage] = useState(false)
   // const blockWidth = (width - 160 - 140 - (2 * 63)) / 64
   const svgRef = useRef<SVGSVGElement>(null)
   const svgRef2 = useRef<SVGSVGElement>(null)
@@ -1403,9 +1404,9 @@ const Trade = () => {
               key: 'Total Cost',
               value: '0.00 USDC'
             },
-          ].map((item, index) => <div key={index} className="flex justify-between md:mb-[16px] mb-[8px] mt-[20px]">
-            <div className="md:text-[20px] text-[14px] font-[400] text-black/30 poppins">{item.key}</div>
-            <div className="md:text-[20px] text-[14px] font-[400] text-black/90 poppins">{item.value}</div>
+          ].map((item, index) => <div key={index} className="flex justify-between mb-[8px]">
+            <div className="md:text-[16px] text-[14px] font-[400] text-black/30 poppins">{item.key}</div>
+            <div className="md:text-[16px] text-[14px] font-[400] text-black/90 poppins">{item.value}</div>
           </div>)
         }
         {/* line */}
@@ -1413,8 +1414,8 @@ const Trade = () => {
         <div className="md:text-[16px] text-[14px] font-[400] text-black/30 poppins md:mt-[16px] mt-[8px]">You’re placing an order to buy 3 share(s) of NATHM1898 at 1.00 price. </div>
         <div
           onClick={() => {
-            // setConfirmBuy(false)
-            router.push('/result')
+            setConfirmBuy(false)
+            setToSuccessPage(true)
           }}
           className="h-[48px] cursor-pointer rounded-[12px] bg-black text-white text-[18px] font-[500] poppins text-center mt-[40px] flex items-center justify-center hover:bg-[#474747]">
           Confirm
@@ -1422,7 +1423,79 @@ const Trade = () => {
         <div className="md:hidden h-[24px]"></div>
       </div>
     </div>}
-  </div>
+    {toSuccessPage && <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex flex-col items-center justify-center transition-all duration-300">
+      <div
+        onClick={() => {
+          setToSuccessPage(false)
+        }}
+        className="flex items-center justify-center absolute top-0 left-0 w-full h-full"></div>
+      <div className="max-md:flex-1" />
+      <div className={`bg-white md:px-[32px] px-[20px] py-[0] md:py-[32px] md:w-[480px] w-full md:rounded-[16px] rounded-t-[16px] transition-all duration-300 ${toSuccessPage ? 'scale-100' : 'scale-0'}`}>
+        <div className="md:hidden flex h-[36px] items-center justify-center" onClick={() => {
+          setToSuccessPage(false)
+        }}>
+          <div className="w-[32px] h-[4px] bg-black-0-1 rounded-full flex items-center justify-center"></div>
+        </div>
+        <div className="flex flex-col mb-[40px] mt-[20px] items-center justify-center">
+          <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14 25L20 31L34 17" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+            <path d="M24 44C35.0456 44 44 35.0456 44 24C44 12.9543 35.0456 4 24 4C12.9543 4 4 12.9543 4 24C4 35.0456 12.9543 44 24 44Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+          <div className="text-[24px] font-[400] VictorSherif leading-[48px] text-black">Order <span className="font-[500] VictorSherif italic">Completed</span></div>
+        </div>
+        <div className="h-[1px] bg-black-0-1 w-full mb-[20px]"></div>
+        {
+          [
+            {
+              key: 'Pair',
+              value: 'NATHM1898 '
+            },
+            {
+              key: 'Type',
+              value: 'Presale / Buy'
+            },
+            {
+              key: 'Amount',
+              value: '30'
+            },
+            {
+              key: 'Direction',
+              value: 'Long'
+            },
+            {
+              key: 'Average Cost',
+              value: '1.99 USDC'
+            },
+            {
+              key: 'Fee',
+              value: '0.00 USDC'
+            },
+            {
+              key: 'Total Cost',
+              value: '1.99 USDC'
+            },
+          ].map((item, index) => <div key={index} className="flex justify-between mb-[8px]">
+            <div className="md:text-[16px] text-[14px] font-[400] text-black/30 poppins">{item.key}</div>
+            <div className={`md:text-[16px] text-[14px] font-[400] ${index === 3 ? 'text-[#12B76A]' : 'text-black/90'} poppins`}>{item.value}</div>
+          </div>)
+        }
+        {/* line */}
+        <div className="h-[1px] bg-black-0-1 w-full mt-[12px]"></div>
+        <div
+          onClick={() => {
+            setToSuccessPage(false)
+          }}
+          className="h-[48px] cursor-pointer rounded-[12px] bg-black text-white text-[18px] font-[500] poppins text-center mt-[40px] flex items-center justify-center hover:bg-[#474747]">
+          Done
+        </div>
+        <div className="flex items-center justify-center mt-[24px] ">
+          <a href="#" className="font-[18px] font-[400] text-center text-black/90 poppins">View Transaction</a>
+        </div>
+        <div className="md:hidden h-[24px]"></div>
+      </div>
+    </div>
+    }
+  </div >
 }
 
 export default Trade
